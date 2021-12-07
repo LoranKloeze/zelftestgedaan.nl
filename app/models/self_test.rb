1393476705ba9f621cea8bb9bbb4061c3565a072
Loran Kloeze <loran@freedomnet.nl>
2021-12-07 11:29:56 +0100

    Make start date not longer than 1 month ago

--- app/models/self_test.rb
+++ app/models/self_test.rb
@@ -1,9 +1,15 @@
 class SelfTest < ApplicationRecord
-  validate :done_at_date_cannot_be_in_the_future
+  validate :done_at_date_cannot_be_in_the_future, :done_at_date_cannot_be_far_in_the_past
 
   def done_at_date_cannot_be_in_the_future
     if done_at.present? && done_at > Date.today
       errors.add(:expiration_date, "can't be in the future")
+    end
+  end
+
+  def done_at_date_cannot_be_far_in_the_past
+    if done_at.present? && done_at < Date.today - 1.month
+      errors.add(:expiration_date, "can't be that far in the past")
     end
   end
 end
