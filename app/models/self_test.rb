de3df1bc94fa1a801c34f847bd064f2779656dc7
Loran Kloeze <loran@freedomnet.nl>
2021-12-07 10:53:33 +0100

    Validate done_at date

--- app/models/self_test.rb
+++ app/models/self_test.rb
@@ -1,2 +1,9 @@
 class SelfTest < ApplicationRecord
+  validate :done_at_date_cannot_be_in_the_future
+
+  def done_at_date_cannot_be_in_the_future
+    if done_at.present? && done_at > Date.today
+      errors.add(:expiration_date, "can't be in the future")
+    end
+  end
 end
