d269a2ae0b4aaa5eead7722f5d896caccbd98234
Loran Kloeze <loran@freedomnet.nl>
2021-12-07 09:18:41 +0100

    Privacy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'thanks', to: 'static#thanks'
-  get 'static/privacy'
+  get 'privacy', to: 'static#privacy'
   root 'self_tests#new'
   resources :self_tests, only: %i[new index create]
 end
