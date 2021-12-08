8eb15e982bf02ed4c600a0297c516758e93ceb91
Loran Kloeze <loran@freedomnet.nl>
2021-12-08 16:34:36 +0100

    Add SlackService

---
+++ app/services/slack_service.rb
@@ -0,0 +1,17 @@
+class SlackService
+  class << self
+    def send_msg(msg)
+      return unless webhook_url
+
+      Net::HTTP.post(
+        URI(webhook_url),
+        { text: msg }.to_json,
+        'Content-Type' => 'application/json'
+      ).code == '200'
+    end
+
+    def webhook_url
+      Rails.application.credentials.dig(:slack, :webhook_url)
+    end
+  end
+end
