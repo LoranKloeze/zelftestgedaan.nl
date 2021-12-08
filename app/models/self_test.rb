fdf399ae91445b1136e346d034162612cb531522
Loran Kloeze <loran@freedomnet.nl>
2021-12-08 16:49:13 +0100

    Add Slack update

--- app/models/self_test.rb
+++ app/models/self_test.rb
@@ -2,6 +2,8 @@ class SelfTest < ApplicationRecord
   validate :done_at_date_cannot_be_in_the_future, :done_at_date_cannot_be_far_in_the_past
   validates :done_at, presence: true
 
+  after_create :send_to_slack
+
   scope :grouped_by_done, lambda {
     select('done_at, COUNT(*) as total, COUNT(*) FILTER(WHERE is_positive = true) as total_positive')
       .group(:done_at)
@@ -14,5 +16,12 @@ class SelfTest < ApplicationRecord
 
   def done_at_date_cannot_be_far_in_the_past
     errors.add(:expiration_date, "can't be that far in the past") if done_at.present? && done_at < Date.today - 1.month
+  end
+
+  def send_to_slack
+    return unless Rails.env == 'production'
+
+    status = is_positive ? '🥵' : '😊'
+    SlackService.send_msg("Nieuwe zelftestuitslag toegevoegd: #{done_at} - #{status} ")
   end
 end
