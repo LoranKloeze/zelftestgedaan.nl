a4a11b657c3915f463b1db5d333b5f97cc778b5b
Loran Kloeze <loran@freedomnet.nl>
2021-12-13 18:27:54 +0100

    Nicify Setting.rb

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,13 +4,4 @@ class Setting < RailsSettings::Base
 
   field :tests_limit_per_request, default: 4
   field :new_tests_interval, default: 5.minutes
-
-
-  # Define your fields
-  # field :host, type: :string, default: "http://localhost:3000"
-  # field :default_locale, default: "en", type: :string
-  # field :confirmable_enable, default: "0", type: :boolean
-  # field :admin_emails, default: "admin@rubyonrails.org", type: :array
-  # field :omniauth_google_client_id, default: (ENV["OMNIAUTH_GOOGLE_CLIENT_ID"] || ""), type: :string, readonly: true
-  # field :omniauth_google_client_secret, default: (ENV["OMNIAUTH_GOOGLE_CLIENT_SECRET"] || ""), type: :string, readonly: true
 end
